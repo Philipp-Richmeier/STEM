1fb7cfeaf33190857e5282d35f3423978bd2a661
Philipp-Richmeier <60767593+Philipp-Richmeier@users.noreply.github.com>
2020-02-06 18:41:07 -1000

    Update sqlUpdate.py

--- sqlUpdate.py
+++ sqlUpdate.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
 # Open database connection
-db = mysql.connector.connect(host="192.168.0.131", user="phiric21", passwd="richmeier", db="phiric21")
+db = mysql.connector.connect(host="192.168.0.131", user="phiric21", passwd="password", db="school")
 
 # prepare a cursor object using cursor() method
 cur = db.cursor(dictionary=True)
 
 # create table as per requirement
-sql = "UPDATE students SET age = '12' WHERE name = 'Holy'"
+sql = "UPDATE students SET age = '12' WHERE name = 'John'"
 
 cur.execute(sql)
 

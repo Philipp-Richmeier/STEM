3172986ddf97157f5e6f7b54a82d634f63c71d68
Philipp-Richmeier <60767593+Philipp-Richmeier@users.noreply.github.com>
2020-02-06 18:40:25 -1000

    Update sqlUpdate.php

--- sqlUpdate.php
+++ sqlUpdate.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "phiric21";
-$password = "richmeier";
-$dbname = "phiric21";
+$password = "password";
+$dbname = "school";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);

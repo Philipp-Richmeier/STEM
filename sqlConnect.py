821811acc82307680eb2fa0bcd09ecaced19d18b
Philipp-Richmeier <60767593+Philipp-Richmeier@users.noreply.github.com>
2020-02-06 18:38:21 -1000

    Update sqlConnect.py

--- sqlConnect.py
+++ sqlConnect.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 # Open database connection
-db = mysql.connector.connect(host="192.168.0.131", user="phiric21", passwd="richmeier", db="phiric21")
+db = mysql.connector.connect(host="192.168.0.131", user="phiric21", passwd="password"")
 
 # disconnect from server
 cur.close()

048e6ff229a710967b3148101a1ccb75878a01a6
Philipp-Richmeier <60767593+Philipp-Richmeier@users.noreply.github.com>
2020-02-06 18:37:50 -1000

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "phiric21";
-$password = "richmeier";
+$password = "password";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
